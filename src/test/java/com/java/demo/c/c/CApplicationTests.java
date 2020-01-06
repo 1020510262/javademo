af7e82da405465aad67702c008c504329c33e9a9
ZIZIGitHub <46522255+ZIZIGitHub@users.noreply.github.com>
2020-01-06 09:43:33 +0800

    Initial commit

---
+++ src/test/java/com/java/demo/c/c/CApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.demo.c.c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/java/demo/c/c/controller/HelloControllerTest.java
@@ -0,0 +1,7 @@
+package com.java.demo.c.c.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+
+}
