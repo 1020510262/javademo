a9c6cc00610c7ccdaf80dc65c93a3f9fabf7fd61
“yzizi git config --global user.name “zizi <1020510262@qq.com>
2020-01-08 15:13:50 +0800

    add Dto,ThirdPartySupport

---
+++ src/main/java/com/java/demo/c/c/Dto/GithubDto.java
@@ -0,0 +1,52 @@
+package com.java.demo.c.c.Dto;
+
+//Github数据交换类-OAuth
+//用于登陆Github，接受Github的一些参数。
+public class GithubDto {
+
+    private String client_id;
+    private String client_secret;
+    private String code;
+    private String redirect_uri;
+    private String state;
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getClient_secret() {
+        return client_secret;
+    }
+
+    public void setClient_secret(String client_secret) {
+        this.client_secret = client_secret;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getRedirect_uri() {
+        return redirect_uri;
+    }
+
+    public void setRedirect_uri(String redirect_uri) {
+        this.redirect_uri = redirect_uri;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
