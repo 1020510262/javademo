ddf5dd9cd49664bcfa335d8a8488bb4fdae2e8ea
“yzizi git config --global user.name “zizi <1020510262@qq.com>
2020-01-08 18:29:19 +0800

    add github log in

--- src/main/java/com/java/demo/c/c/ThirdPartySupport/HttpGet.java
+++ src/main/java/com/java/demo/c/c/ThirdPartySupport/HttpGet.java
@@ -1,21 +1,19 @@
 package com.java.demo.c.c.ThirdPartySupport;
 
-import okhttp3.*;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import java.io.IOException;
-//okHttp的POST远程服务器传输工具
-public class HttpPOST {
-    public static final MediaType JSON
-            = MediaType.get("application/json; charset=utf-8");
-
+//okHttp的Get远程服务器传输工具
+public class HttpGet {
     OkHttpClient client = new OkHttpClient();
 
-    public String post(String url, String json) throws IOException {
-        RequestBody body = RequestBody.create(json, JSON);
+    public String run(String url) throws IOException {
         Request request = new Request.Builder()
                 .url(url)
-                .post(body)
                 .build();
+
         try (Response response = client.newCall(request).execute()) {
             return response.body().string();
         }
---
+++ src/main/java/com/java/demo/c/c/controller/GitHubLog.java
@@ -0,0 +1,34 @@
+package com.java.demo.c.c.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.java.demo.c.c.Dto.GithubDto;
+import com.java.demo.c.c.Dto.GithubUserDto;
+import com.java.demo.c.c.ThirdPartySupport.HttpGet;
+import com.java.demo.c.c.ThirdPartySupport.HttpPOST;
+import java.io.IOException;
+//用于处理Git登陆过程的值传递
+public class GitHubLog{
+    private String access_token;
+
+    public void GitHttpPost(String code) throws IOException {
+        GithubDto githubDto = new GithubDto();
+        githubDto.setClient_id("df63d3b995d374a3221a");
+        githubDto.setClient_secret("110346bd3c4ea86e7ea56e6cd5d6ca63937c8608");
+        githubDto.setCode(code);
+        githubDto.setRedirect_uri("http://localhost:8080/callback");
+        githubDto.setState("1");
+        access_token = new HttpPOST().post("https://github.com/login/oauth/access_token" , JSON.toJSONString(githubDto));
+        System.out.println(access_token);
+    }
+
+    public String GitHttpGet() throws IOException {
+        GithubUserDto githubUserDto = new GithubUserDto();
+        String[] strings1 = access_token.split("=");
+        String[] strings2 = strings1[1].split("&");
+        access_token =  strings2[0];
+        String y = new HttpGet().run("https://api.github.com/user?access_token=" + access_token);
+        System.out.println(y);
+        return y;
+    }
+
+}
